7bdd5d275a0b18397cb2de10b78b7d2839f6a728
joseph townshend <joetownshend@mac.com>
2019-10-18 15:51:32 +0100

    feat: checkbox comonent working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Checkbox from './components/Checkbox';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>hello</div>
+        <Checkbox />
       </div>
     );
   }
---
+++ src/components/Checkbox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Checkbox extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="checkbox"/
+        >
+      </div>
+    );
+  }
+}
