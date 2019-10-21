55e809071cd109f130bc35fe0e30ddb98ea05bad
joseph townshend <joetownshend@mac.com>
2019-10-21 19:14:19 +0100

    refactor: button grid created in componenet

--- src/App.js
+++ src/App.js
@@ -4,147 +4,23 @@ import './App.css';
 
 class App extends Component {
 
-  state = {
-    checked: null
-  }
-
-  resetCheckbox = () => {
-    console.log(this.state);
-    this.setState({
-      checked: this.state.checked = false
-    });
-      console.log(this.state);
-  };
+  // state = {
+  //   checked: null
+  // }
+  //
+  // resetCheckbox = () => {
+  //   console.log(this.state);
+  //   this.setState({
+  //     checked: this.state.checked = false
+  //   });
+  //     console.log(this.state);
+  // };
 
   render() {
     return (
-      <React.Fragment>
-        <div className="Grid">
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div className="Row">
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-            <div className="Box"><Checkbox /></div>
-          </div>
-          <div>
-            <button onClick={this.resetCheckbox}>reset</button>
-          </div>
-        </div>
-      </React.Fragment>
+      <div className="App">
+        <Checkbox />
+      </div>
     );
   }
 }
--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import '../App.css';
 
 export default class Checkbox extends React.Component {
   render() {
     return (
-      <div>
-        <input
-          type="checkbox"/
-        >
-      </div>
+      <React.Fragment>
+        <div className="Grid">
+          <div className="Row">
+            <div className="Box"><input type="checkbox"/></div>
+            <div className="Box"><input type="checkbox"/></div>
+            <div className="Box"><input type="checkbox"/></div>
+            <div className="Box"><input type="checkbox"/></div>
+            <div className="Box"><input type="checkbox"/></div>
+          </div>
+          <div className="Row">
+            <div className="Box"><input type="checkbox"/></div>
+            <div className="Box"><input type="checkbox"/></div>
+            <div className="Box"><input type="checkbox"/></div>
+            <div className="Box"><input type="checkbox"/></div>
+            <div className="Box"><input type="checkbox"/></div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
